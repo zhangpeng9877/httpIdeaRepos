8c463ca2193359309e5b56ecfaeacc4b71593150
zp9877 <169906224@qq.com>
2020-06-20 20:22:58 +0800

    初始化项目de 3.0

--- test/src/main/java/com/itheima/test/Test.java
+++ test/src/main/java/com/itheima/test/Test.java
@@ -8,5 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我在练习git，两年半了...");
         System.out.println("这是被修改的克隆项目...");
+        System.out.println("这是被修改的克隆项目...");
+
     }
 }

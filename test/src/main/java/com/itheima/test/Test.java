ab750cddadd3200733bbb4a2302de11fae18fdf9
zp9877 <169906224@qq.com>
2020-06-20 19:56:20 +0800

    初始化项目

---
+++ test/src/main/java/com/itheima/test/Test.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+/**
+ * @author 张鹏
+ * @date 2020/6/20 19:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我在练习git，两年半了...");
+    }
+}
